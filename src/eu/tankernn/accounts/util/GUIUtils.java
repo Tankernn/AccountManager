82e03d87d9271c2eeed9e46f98c9957a2e20c9bb
Tankernn <frans@tankernn.eu>
2016-12-09 10:10:53 +0100

    Better list to GUI conversion

--- src/eu/tankernn/accounts/util/GUIUtils.java
+++ src/eu/tankernn/accounts/util/GUIUtils.java
@@ -9,15 +9,13 @@ import javax.swing.DefaultListModel;
 public class GUIUtils {
 	public static <T> DefaultListModel<T> listModelFromList(List<T> list) {
 		DefaultListModel<T> model = new DefaultListModel<T>();
-		for (T a : list)
-			model.addElement(a);
+		list.forEach(t -> model.addElement(t));
 		return model;
 	}
 	
 	public static <T> ComboBoxModel<T> comboBoxModelFromList(List<T> list) {
 		DefaultComboBoxModel<T> model = new DefaultComboBoxModel<T>();
-		for (T a : list)
-			model.addElement(a);
+		list.forEach(t -> model.addElement(t));
 		return model;
 	}
 }
